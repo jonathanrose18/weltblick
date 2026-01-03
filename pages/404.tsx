276244d31494355bd880f4b22a89112474476c8d
Jonathan Rose <jonathan.rose@techstube.io>
2026-01-03 21:00:09 +0100

    feat:  update 404 page component

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,29 +1,45 @@
-import Link from 'next/link';
-import { ArrowUpRightIcon, CircleQuestionMarkIcon } from 'lucide-react';
-import type { ReactElement } from 'react';
+import Link from "next/link";
+import { ArrowUpRightIcon, CircleQuestionMarkIcon } from "lucide-react";
+import type { ReactElement } from "react";
 
-import { Button } from '@/shared/components/ui/button';
-import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '@/shared/components/ui/empty';
-import { Layout } from '@/shared/components/layout';
+import { Button } from "@/shared/components/ui/button";
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/shared/components/ui/empty";
+import { Layout } from "@/shared/components/layout";
 
-export default function NotFound() {
+import type { NextPageWithLayout } from "./_app";
+
+const NotFound: NextPageWithLayout = () => {
   return (
     <Empty>
       <EmptyHeader>
-        <EmptyMedia variant='icon'>
+        <EmptyMedia variant="icon">
           <CircleQuestionMarkIcon />
         </EmptyMedia>
         <EmptyTitle>404 | Page not found</EmptyTitle>
-        <EmptyDescription>This page disappeared into the digital abyss.</EmptyDescription>
+        <EmptyDescription>
+          This page disappeared into the digital abyss.
+        </EmptyDescription>
       </EmptyHeader>
-      <Button asChild className='text-muted-foreground' size='sm' variant='link'>
-        <Link href='/' prefetch={false}>
+      <Button
+        asChild
+        className="text-muted-foreground"
+        size="sm"
+        variant="link"
+      >
+        <Link href="/" prefetch={false}>
           Back to home <ArrowUpRightIcon />
         </Link>
       </Button>
     </Empty>
   );
-}
-NotFound.getLayout = function getLayout(page: ReactElement) {
-  return <Layout>{page}</Layout>;
 };
+
+NotFound.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
+
+export default NotFound;
